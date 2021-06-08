3e9e951c2ee937a682e5ea7b1ec747d545afd677
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2021-06-09 01:18:31 +0700

    add aydao config

--- training/networks.py
+++ training/networks.py
@@ -438,7 +438,7 @@ class SynthesisNetwork(torch.nn.Module):
         self.img_resolution_log2 = int(np.log2(img_resolution))
         self.img_channels = img_channels
         self.block_resolutions = [2 ** i for i in range(2, self.img_resolution_log2 + 1)]
-        channels_dict = {res: min(channel_base // res, channel_max) for res in self.block_resolutions}
+        channels_dict = {res: min((channel_base // res) * 2, channel_max) for res in self.block_resolutions}
         fp16_resolution = max(2 ** (self.img_resolution_log2 + 1 - num_fp16_res), 8)
 
         self.num_ws = 0
